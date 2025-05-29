e584f8efacc8944dddbeae8e4191e3baf0d7af91
AlinaKozak <kozakalina09@gmail.com>
2025-05-30 00:21:21 +0300

    9 lab tack1

--- src/Lab9/tack1/Main.java
+++ src/Lab9/tack1/Main.java
@@ -2,6 +2,10 @@ package Lab9.tack1;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Calculator obj = new Calculator();
+
+        System.out.println(obj.add(2, 3));
+        System.out.println(obj.add(1.5, 9.3));
+        System.out.println(obj.add(10, 34, 98));
     }
 }
